8ff7bc1d3bcab76b043b04c1ea52cf3f40eb46d8
TaeGyung Gong <gtg92t@gmail.com>
2020-01-24 01:09:04 +0900

    1928. Base64 Decoder

--- SWEA/D2/SWEA_1928.py
+++ SWEA/D2/SWEA_1928.py
@@ -2,45 +2,42 @@
 # 8 bit 문자 = > 6 bit 문자
 # Decoding
 
-base64_dict_binary = dict()
-base64_dict_digit = dict()
+def decode_String(string):
+    result_string = ''
+    for idx in range(len(string) // 4):
+        base64_string = string[idx * 4:(idx + 1) * 4]
+        decoding = 0
+        temp_list = list()
+        for index in range(4):
+            value = get_decode_Value(base64_string[index])
+            decoding = (decoding << 6) + value
 
-for a in range(1,27) :
-    if chr(a+64) not in base64_dict_digit :
-        base64_dict_digit[chr(a+64)] = a-1
-for b in range(1,27) :
-    if chr(b+96) not in base64_dict_digit :
-        base64_dict_digit[chr(b+96)] = b+25
-for c in range(0,10) :
-    if chr(c+48) not in base64_dict_digit :
-        base64_dict_digit[chr(c+48)] = c+52
-base64_dict_digit['+'] = 62
-base64_dict_digit['/'] = 63
+        temp_list.append(chr(decoding >> 16))
+        temp_list.append(chr((decoding >> 8) & 0xff))
+        temp_list.append(chr(decoding & 0xff))
+        temp_string = ''.join(map(str,temp_list))
+        result_string = result_string + temp_string
+    return result_string
+
+def get_decode_Value(alpha):
+
+    if ('A' <= alpha <= 'Z'):
+        val = ord(alpha) - ord('A')
+    elif ('a' <= alpha <= 'z'):
+        val = ord(alpha) - ord('a') + 26
+    elif ('0' <= alpha <= '9'):
+        val = ord(alpha) - ord('0') + 52
+    elif (alpha == '+'):
+        val = 62
+    elif (alpha == '/'):
+        val = 63
+    return val
 
 
-for i in range(1,27) :
-    if chr(i+64) not in base64_dict_binary :
-        base64_dict_binary[chr(i+64)] = str(bin(i-1))[2:]
-for k in range(1,27) :
-    if chr(k+96) not in base64_dict_binary :
-        base64_dict_binary[chr(k+96)] = str(bin(k+25))[2:]
-for j in range(0,10) :
-    if chr(j+48) not in base64_dict_binary :
-        base64_dict_binary[chr(j+48)] = str(bin(j+52))[2:]
-base64_dict_binary['+'] = str(bin(62))[2:]
-base64_dict_binary['/'] = str(bin(63))[2:]
 
-result = ''
-my_string = input()
-for string in my_string :
-    if string in base64_dict_binary.keys() :
-        result += base64_dict_binary[string]
-
-result_list =list()
-for i in range(1,(len(result))//6+1) :
-    result_list.append(result[6*(i-1):6*i])
-
-for i,(key,value) in enumerate(base64_dict_digit.items()):
-    if value == int('0b'+str(result_list[0]),2) :
-        print(key)
-
+test_cnt = int(input())
+result_decode = ''
+for test_count in range(1, test_cnt+1):
+    encode_string = input()
+    result_decode = decode_String(encode_string)
+    print('#{} {}'.format(test_count,result_decode))
