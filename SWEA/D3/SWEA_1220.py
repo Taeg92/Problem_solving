308547da4618f5e6e014f74b17e3dc1e152f79b8
TaeGyung Gong <gtg92t@gmail.com>
2020-01-27 22:09:20 +0900

    1220. Magnetic

--- SWEA/D3/SWEA_1220.py
+++ SWEA/D3/SWEA_1220.py
@@ -3,7 +3,7 @@
 # 첫째 줄에는 정사각형 테이블의 한변의 길이가 입력, 그 다음 줄에는 테스트 케이스
 # 1 : N극 , 2 : S극
 
-for test in range(10) :
+for test in range(1,11) :
     result = 0
     size = int(input())
     matrix = [list(map(int,input().split())) for _ in range(size)] 
@@ -18,4 +18,5 @@ for test in range(10) :
                 flag = 0
                 result += 1
             idx += 1
+        flag = 0
     print('#{} {}'.format(test,result))
