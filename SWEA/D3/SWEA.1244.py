7ba5a034bad38755cd7c486ea661705971b0fca9
TaeGyung Gong <gtg92t@gmail.com>
2020-01-30 01:06:50 +0900

    1244. 최대상금

--- SWEA/D3/SWEA.1244.py
+++ SWEA/D3/SWEA.1244.py
@@ -1,38 +1,37 @@
-# Problem [1244] : 최대상금
-# 각 테스트 케이스에 대해 최대 6자리 정수형 숫자와 교환 횟수를 입력받는다.
+# # Problem [1244] : 최대상금
+# # 각 테스트 케이스에 대해 최대 6자리 정수형 숫자와 교환 횟수를 입력받는다.
 
-def find_max(string,cnt) :
-    max_num = 0
-    num_list = list(map(int,string))
-    start = 0
-    flag = 0
-    while True:
-        if flag == 0 :
-            max_num = max(num_list)
-            max_idx = [i for i,val in enumerate(num_list) if val == max_num]
-        else :
-            temp_list = num_list[start:]
-            max_num = max(temp_list)
-            max_idx = [j for j,value in enumerate(num_list) if value == max_num]
-        if max_idx[-1] == start :
-            start += 1
-            temp_list = num_list[start:]
-            max_num = max(temp_list)
-            max_idx = [k for k,v in enumerate(num_list) if v == max_num]
-        idx = max_idx.pop(-1)
-        temp = num_list[idx]
-        num_list[idx] = num_list[start]
-        num_list[start] = temp
-        flag = 1
-        start += 1
+def find_max(arr,cnt) :
+    original = list(sorted(arr[0]))[::-1]
+    original_max = int(''.join(map(str,original)))
+    temp = list()
+    while cnt > 0 :
+        for array in arr:
+            for i in range(len(array)-1):
+                for j in range(i+1,len(array)):
+                    copy_arr = array[:]
+                    copy_arr[i] , copy_arr[j] = copy_arr[j], copy_arr[i]
+                    if copy_arr not in temp :
+                        temp.append(copy_arr[:])
+        arr.extend(temp)
         cnt -= 1
-        if cnt == 0 :
-            return num_list      
+        
+        max_num = max([int(''.join(map(str,lst))) for lst in arr])
+        
+        if max_num == original_max and cnt != 0:
+            max_list = original
+            for _ in range(cnt):
+                max_list[-1], max_list[-2] = max_list[-2], max_list[-1]
+            return int(''.join(map(str,max_list)))
+    
+    my_result = [int(''.join(map(str,lst))) for lst in arr]
+    return max(my_result)
+        
+
 
 test_cnt = int(input())
 for test in range(1,test_cnt+1) :
     numbers, change_cnt = input().split()
     cnt = int(change_cnt)
-    result = find_max(numbers,cnt)
-    num = ''.join(map(str,result))
-    print('#{} {}'.format(test,num))
+    result = find_max([list(numbers)],cnt)
+    print('#{} {}'.format(test,result))
