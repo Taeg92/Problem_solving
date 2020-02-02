d113cffec70c33c4fe191caca4811a4273905de8
TaeGyung Gong <gtg92t@gmail.com>
2020-02-03 00:22:32 +0900

    15649. N과 M(1)

---
+++ Baekjoon/Backtracking/BOJ_15649.py
@@ -0,0 +1,28 @@
+# Problem [14539] : N과 M(1)
+
+def permutation(arr, r):
+    # 1.
+    result = list()
+    arr = sorted(arr)
+    used = [0 for _ in range(len(arr))]
+
+    def generate(chosen, used):
+        # 2.
+        if len(chosen) == r:
+            result.append(chosen[:])
+	
+	# 3.
+        for i in range(len(arr)):
+            if not used[i]:
+                chosen.append(arr[i])
+                used[i] = 1
+                generate(chosen, used)
+                used[i] = 0
+                chosen.pop()
+    generate([], used)
+    return result
+
+n_val, m_val = map(int, input().split())
+result = permutation(range(1,n_val+1), m_val)
+for i in result:
+    print(' '.join(map(str,i)))
