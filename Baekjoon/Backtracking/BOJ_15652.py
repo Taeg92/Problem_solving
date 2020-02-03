2de2acefca3af5cc9f303764e8f361155361e771
TaeGyung Gong <gtg92t@gmail.com>
2020-02-03 20:48:17 +0900

    15652. N과 M(4)

---
+++ Baekjoon/Backtracking/BOJ_15652.py
@@ -0,0 +1,13 @@
+def repeat_conb(arr,r):
+    for i in range(len(arr)):
+        if r == 1:
+            yield [arr[i]]
+        else:
+            for next in repeat_conb(arr[i:],r-1):
+                yield [arr[i]] + next
+
+
+N, M = map(int,input().split())
+result = list(repeat_conb(range(1,N+1),M))
+for n in result:
+    print(' '.join(map(str,n)))
