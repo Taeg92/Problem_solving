b5fafdbc9f86be9541a7c2e1380dbae79c1ba3a2
TaeGyung Gong <gtg92t@gmail.com>
2020-02-03 20:47:19 +0900

    15651. N과 M(3)

---
+++ Baekjoon/Backtracking/BOJ_15651.py
@@ -0,0 +1,13 @@
+def product(arr,r):
+    for i in range(len(arr)):  
+        if r == 1:  
+            yield [arr[i]]
+        else:
+            for next in product(arr,r-1): 
+                yield [arr[i]] + next
+
+
+N, M = map(int,input().split())
+result = list(product(range(1,N+1),M))
+for n in result:
+    print(' '.join(map(str,n)))
