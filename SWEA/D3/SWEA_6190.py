463a488aed41bdbfe0b78a5db648da9afc0ced2e
TaeGyung Gong <gtg92t@gmail.com>
2020-02-05 20:10:38 +0900

    6190. 정곤이의 단조 증가하는 수

--- SWEA/D3/SWEA_6190.py
+++ SWEA/D3/SWEA_6190.py
@@ -3,51 +3,28 @@
 # 입력 받는 숫자의 개수 N
 # N개의 수를 입력
 
-# def is_Increase(arr,n):
-#     global prev
-#     if n == N:
-#         return
-#     else:
-#         for i in range(N):
-#             if arr[n]*arr[i] > prev:
-#                 prev = arr[n]*arr[i]
-#                 is_Increase(arr,n+1)
-
-
-
-# T = int(input())
-# for tc in range(1,T+1):
-#     N = int(input())
-#     numbers = [list(map(int,input().split())) for _ in range(N)]
-#     prev = 0
-#     is_Increase(numbers,0)
-#     print(prev)
-def chk_danjo(num):
-    while num > 0:
-        num, r = divmod(num, 10)
-        if not num % 10 <= r:
+def is_Increase(n):
+    while n > 0:
+        n, r = divmod(n, 10)
+        if not n % 10 <= r:
             return False
     return True
 
-def dfs(level, selected, product):
-    global max_result
-    if selected == 2:
-        if product < max_result: return
-        if chk_danjo(product):
-            max_result = product
-            return
-    if level == n: 
-        return
-    dfs(level+1, selected + 1, product * ns[level])
-    dfs(level+1, selected, product)
-    
+def find_max(arr) :
+    m = 0
+    for i in range(N-1):
+        for j in range(i+1,N):
+            if is_Increase(data[i]*data[j]):
+                if m < data[i]*data[j]:
+                    m = data[i]*data[j]
+    if m == 0:
+        return -1 
+    return m
+
+
 T = int(input())
-for t in range(1, T+1):
-    n = int(input())
-    ns = list(map(int, input().split()))
-    max_result = 0
-    dfs(0, 0, 1)
-    if max_result == 0:
-        print("#{} {}".format(t, -1))
-    else:
-        print("#{} {}".format(t, max_result))
+for tc in range(1, T+1):
+    N = int(input())
+    data = sorted(list(map(int, input().split())),reverse=True)
+    result = find_max(data)
+    print('#{} {}'.format(tc,result))
