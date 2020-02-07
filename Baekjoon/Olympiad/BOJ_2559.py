90a54b191cddd4774ae54902c6e2c0f6eb1b41d7
TaeGyung Gong <gtg92t@gmail.com>
2020-02-08 02:00:46 +0900

    2559. 수열

--- Baekjoon/Olympiad/BOJ_2559.py
+++ Baekjoon/Olympiad/BOJ_2559.py
@@ -2,11 +2,28 @@
 # 입력 첫째 줄에 정수 N과 K가 주어진다. N : 날짜수 K :연속 수
 # 입력 둘째 줄에는 측정한 온도들이 N개 주어진다.
 
+def Prefix_Sum(arr,K):
+    section_sum = sum(arr[0:K])
+    m = section_sum
+    i = 0
+    if K == 1:
+        return max(arr)
+    else:
+        while 1:
+            if i + K > N-1:
+                break
+            section_sum -= arr[i]
+            section_sum += arr[i+K]
+            i += 1
+            if section_sum > m:
+                m = section_sum
+        return m
+            
+
 
 N, K = map(int,input().split())
 data = list(map(int,input().split()))
-m = 0
-for i in range(len(data)-K):
-    if m < sum(data[i:i+K]):
-        m = sum(data[i:i+K])
+
+m = Prefix_Sum(data,K)
 print(m)
+
