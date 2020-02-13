68143eee5359f778df923af19e8ff4f2126b04da
TaeGyung Gong <gtg92t@gmail.com>
2020-02-14 00:03:38 +0900

    Iron rod

--- Programmers/Stack/Iron_rod.py
+++ Programmers/Stack/Iron_rod.py
@@ -1,2 +1,26 @@
-# Problem : Iron_rod
+# Problem : 쇠막대기
 
+from collections import deque
+
+def solution(arrangement):
+    stack = deque()
+    arr = arrangement.replace('()','L')
+    rod = deque(arr)
+    answer = 0
+    while len(rod) != 0:
+        temp = rod.popleft()
+        if temp == '(' :
+            stack.append(temp)
+            answer += 1      
+        elif temp == 'L':
+            answer += len(stack)
+        else:
+           stack.pop()
+    return answer
+
+
+arrangement = '()(((()())(())()))(())'
+print(solution(arrangement))
+
+arrangement = '(())'
+print(solution(arrangement))
