998a9b43dd9f2f0901f2d75cb1963421d43dabdf
TaeGyung Gong <gtg92t@gmail.com>
2020-02-14 20:03:54 +0900

    1075. Z

---
+++ Baekjoon/Recursion/BOJ_1074.py
@@ -0,0 +1,22 @@
+# Problem [1074] : Z
+
+def recursion_Z(r, c):
+    
+    # 종료조건
+    if r == 0 and c == 0:
+        return 0
+
+    else:
+        if r % 2 == 1:
+            if c % 2 == 1:
+                return recursion_Z()
+            else:
+        else:
+            if c % 2 == 1:
+
+
+
+
+N, r, c = map(int, input().split())
+result = recursion_Z(r,c)
+print(result)
