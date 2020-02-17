2d05be0c8f7c3fc0e305bc87ceff4bde9ffc4605
TaeGyung Gong <gtg92t@gmail.com>
2020-02-18 00:30:20 +0900

    15686. 치킨 배달

--- Baekjoon/DFS/BOJ_15686/BOJ_15686.py
+++ Baekjoon/DFS/BOJ_15686/BOJ_15686.py
@@ -14,18 +14,20 @@ def combination(arr,r):
 N, M = map(int,input().split())
 data = [list(map(int,input().split())) for _ in range(N)]
 
-home = [[i,j] for i in range(N) for j in range(N) if data[i][j] == 1]
-chicken = [[i,j] for i in range(N) for j in range(N) if data[i][j] == 2]
+home = [[i+1,j+1] for i in range(N) for j in range(N) if data[i][j] == 1]
+chicken = [[i+1,j+1] for i in range(N) for j in range(N) if data[i][j] == 2]
 choice_chicken = list(combination(chicken,M))
-min_dist = 0
+min_s = 0
 for choice in choice_chicken:
-    dist = 0
-    for i in range(M):
-        for j in range(len(home)):
-            dist += abs(choice[i][0]-home[j][0]) +abs(choice[i][1]-home[j][1])
-    if min_dist == 0:
-        min_dist = dist
+    s = 0
+    for i in range(len(home)):
+        dist = list()
+        for j in range(M):
+            dist.append(abs(choice[j][0]-home[i][0]) +abs(choice[j][1]-home[i][1]))
+        s += min(dist)
+    if min_s == 0:
+        min_s = s
     else:
-        if min_dist > dist:
-            min_dist = dist
-print(min_dist)
+        if min_s > s:
+            min_s = s
+print(min_s)
