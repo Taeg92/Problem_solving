848a96fd7c11d6b4da7ee3bfa5bff58570cab41c
Taegyung Gong <gtg92t@gmail.com>
2020-02-18 20:49:34 +0900

    13023. ABCDE

--- Baekjoon/DFS/BOJ_13023/BOJ_13023.py
+++ Baekjoon/DFS/BOJ_13023/BOJ_13023.py
@@ -4,15 +4,36 @@ import sys
 sys.stdin = open('input.txt')
 
 
-T = int(input())
-for tc in range(1,T+1):
-    V, E = map(int,input().split())
-    G = [[] for _ in range(V)]
-    result = list()
-    for _ in range(E):
-        u, v = map(int, input().split())
-        G[u].append(v)
-        G[v].append(u)
-    check = [0]*V
-    print(G)
+def dfs(node, depth):
+    global possible
+    visited[node] = 1
+    if (depth == 4):
+        possible = True
+        return
+    for x in v[node]:
+        if visited[x] == 0:
+            dfs(x, depth+1)
+            if (possible):
+                return
+    visited[node] = 0
+
+
+if __name__ == "__main__":
+    N, M = map(int,input().split())
+    v = [[] for _ in range(N)]
+    visited = [0]*N
+    possible = False
+
+
+    for _ in range(M):
+        a, b = map(int,input().split())
+        v[a].append(b)
+        v[b].append(a)
     
+    for i in range(N):
+        dfs(i, 0)
+        if (possible):
+            break
+    if (possible):
+        print(1)
+    else: print(0)
