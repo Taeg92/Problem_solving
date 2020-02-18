bdecf0a112b601f6f35f08411091737c808ef0d3
Taegyung Gong <gtg92t@gmail.com>
2020-02-18 20:08:07 +0900

    1926. 그림

--- Baekjoon/DFS/BOJ_1926/BOJ_1926.py
+++ Baekjoon/DFS/BOJ_1926/BOJ_1926.py
@@ -1,39 +1,36 @@
 # Problme [1926] : 그림
 
 import sys
-sys.setrecursionlimit(500*500)
+sys.setrecursionlimit(1000*1000)
 sys.stdin = open('input.txt')
 
+
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
 def DFS(col,row):
-    global area
     check[col][row] = 1
-    dx = [-1, 0, 1, 0]
-    dy = [0, 1, 0, -1]
+    ret = 1
 
     for i in range(4):
         x = col + dx[i]
         y = row + dy[i]
         if 0 <= x < N and 0 <= y < M:
             if not check[x][y] and d[x][y] == 1:
-                check[x][y] = 1
-                area += 1
-                area_list.append(area)
-                DFS(x,y)
+                ret += DFS(x,y)
+    
+    return ret
+
 
 N, M = map(int,input().split())
 d = [list(map(int,input().split())) for _ in range(N)]
 check = [[0]*M for _ in range(N)]
-area_list = list()
 cnt = 0
 m = 0
 for i in range(N):
-    area = 1
     for j in range(M):
         if d[i][j] == 1 and not check[i][j]:
-            DFS(i,j)
+            m = max(m, DFS(i,j))
             cnt += 1
-    area_list.append(area)
-    if max(area_list) > m:
-        m = max(area_list) 
 print(cnt)
 print(m)
