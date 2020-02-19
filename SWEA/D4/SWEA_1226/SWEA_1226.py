8532187ddcae33126d8a2ee6e6850996f8d23671
TaeGyung Gong <gtg92t@gmail.com>
2020-02-19 15:20:42 +0900

    1226. 미로1

---
+++ SWEA/D4/SWEA_1226/SWEA_1226.py
@@ -0,0 +1,35 @@
+# Problem [1226] : 미로1
+
+import sys
+sys.stdin = open('input.txt')
+
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+def BFS(x,y):
+    Possible = 0
+    Queue = list()
+    check[x][y] = 1
+    Queue.append([x,y])
+
+    while Queue:
+
+        cx, cy = Queue.pop(0)
+        if data[cx][cy] == 3:
+            Possible = 1
+
+        for i in range(4):
+            nx = cx + dx[i]
+            ny = cy + dy[i]
+            if (1 <= nx <= 15) and (1 <= ny <= 15):
+                if not check[nx][ny] and data[nx][ny] == 0 or data[nx][ny] == 3:
+                    Queue.append([nx,ny])
+                    check[nx][ny] = 1
+    return Possible
+
+for tc in range(1, 11):
+    T = int(input())
+    data = [list(map(int,list(input()))) for _ in range(16)]
+    check = [[0]*(16) for _ in range(16)]
+    result = BFS(1,1)
+    print('#{} {}'.format(T,result))
