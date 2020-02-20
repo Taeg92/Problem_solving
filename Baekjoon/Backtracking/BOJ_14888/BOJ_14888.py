8c985397a5b07ec416be8a40219e7b7b58744b21
TaeGyung Gong <gtg92t@gmail.com>
2020-02-20 13:54:17 +0900

    14888. 연산자 끼워넣기

--- Baekjoon/Backtracking/BOJ_14888/BOJ_14888.py
+++ Baekjoon/Backtracking/BOJ_14888/BOJ_14888.py
@@ -1,52 +1,39 @@
 # Problem [14888] : 연산자 끼워넣기
 
-def permutation(arr,r):
-    result = list()
-    arr = sorted(arr)
-    used = [0]*len(arr)
+import sys
+sys.stdin = open('input.txt')
 
-    def generate(chosen,used):
+min_val = sys.maxsize
+max_val = -sys.maxsize
 
+def BT(n,s):
+    # [+, -, X, /]
+    global min_val, max_val
+    if n == N:
+        min_val = min(min_val, s)
+        max_val = max(max_val, s)
+    
+    else:
+        for i in range(4):
+            if operator[i]:
+                operator[i] -= 1
+                if i == 0:
+                    BT(n+1, s + d[n])
+                elif i == 1:
+                    BT(n+1, s - d[n])
+                elif i == 2:
+                    BT(n+1, s * d[n])
+                else:
+                    if s < 0:
+                        BT(n+1, -(abs(s)//d[n]))
+                    else:
+                        BT(n+1, s // d[n])
+                operator[i] += 1
 
-        if len(chosen) == r:
-            result.append(chosen[:])
-
-        for i in range(len(arr)):
-            if not used[i]:
-                chosen.append(arr[i])
-                used[i] = 1
-                generate(chosen,used)
-                used[i] = 0
-                chosen.pop()
-    generate([],used)
-    return result
-
-
-operator = ['+', '-', '*', '//']
-
-N = int(input())
-data = list(map(int,input().split()))
-num_oper = list(map(int,input().split()))
-m = list()
-oper = list()
-for i in range(4):
-    if num_oper[i] != 0:
-        for _ in range(num_oper[i]):
-            oper.append(operator[i])
-
-choice = permutation(oper,N-1)
-for c in choice:
-    s = data[0]
-    for i in range(N-1):
-        if c[i] == '+':
-            s = s + data[i+1]
-        elif c[i] == '-':
-            s = s - data[i+1]
-        elif c[i] == '*':
-            s = s * data[i+1]
-        else:
-            s = abs(s) // data[i + 1]
-    m.append(s)
-
-print(max(m))
-print(min(m))
+if __name__ == '__main__':
+    N = int(input())
+    d = list(map(int,input().split()))
+    operator = list(map(int,input().split()))
+    BT(1, d[0])
+    print(max_val)
+    print(min_val)
