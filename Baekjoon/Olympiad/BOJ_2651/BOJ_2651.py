7b7550440cb46eaa4a0c38b34b9ef690ce643902
TaeGyung Gong <gtg92t@gmail.com>
2020-02-20 18:13:46 +0900

    2651. 자동차경주대회

--- Baekjoon/Olympiad/BOJ_2651/BOJ_2651.py
+++ Baekjoon/Olympiad/BOJ_2651/BOJ_2651.py
@@ -1,20 +1,20 @@
 # Problem [2651] : 자동차경주대회
 
 import sys
-sys.stdin = open('input.txt')
+# sys.stdin = open('input.txt')
 m = sys.maxsize
 def DFS(v,d,t):
     global m
+    if d < 0 :
+        return
     if v == N:
         m = min(m,t)
-        
     for i in range(v,N):
+
         if not check[i]:
-            if d > data[i]:
-                DFS(v+1,d - data[i],t + T[i])
-            else:
-                DFS(v-1,dist,t)
-                check[i] = 1
+            check[i] = 1
+            DFS(i+1,dist,t+T[i])
+            check[i] = 0
 
 
 if __name__ == '__main__':
@@ -23,5 +23,5 @@ if __name__ == '__main__':
     data = list(map(int, input().split()))
     T = list(map(int, input().split()))
     check = [0]*N
-    DFS(0,0,0)
+    DFS(0,dist,0)
     print(m)
