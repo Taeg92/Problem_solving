ff33a31d4e2274982b857bfcc98e82e8c122602c
TaeGyung Gong <gtg92t@gmail.com>
2020-02-22 00:02:50 +0900

    Still solving

---
+++ Baekjoon/Backtracking/BOJ_16637/BOJ_16637.py
@@ -0,0 +1,12 @@
+# Problem [16637] : 괄호 추가하기
+
+import sys
+input = lambda : sys.stdin.readline().rstrip()
+sys.stdin = open('input.txt')
+
+
+
+
+if __name__ == "__main__":
+    N = int(input())
+    D = list(map(int,input().split()))
