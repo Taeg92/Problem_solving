52de8b8387c4e37119d8d0594596cbc2fe7ca92d
Taegyung Gong <gtg92t@gmail.com>
2020-02-24 17:56:08 +0900

    Still solving

---
+++ Baekjoon/Backtracking/BOJ_17281/BOJ_17281.py
@@ -0,0 +1,13 @@
+# Problem [17281] : 야구
+
+import sys
+sys.stdin = open('input.txt')
+
+
+
+
+if __name__ == '__main__':
+    N = int(input())
+    s = 0
+    D = [list(map(int,input().split())) for _ in range(N)]
+    print(s)
