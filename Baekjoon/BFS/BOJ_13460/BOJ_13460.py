34fef907f2e6da078c9a9df9f128fb620471f83c
TaeGyung Gong <gtg92t@gmail.com>
2020-03-04 00:31:41 +0900

    Still solving

--- Baekjoon/BFS/BOJ_13460/BOJ_13460.py
+++ Baekjoon/BFS/BOJ_13460/BOJ_13460.py
@@ -3,8 +3,11 @@
 import sys
 sys.stdin = open('input.txt')
 
+def DFS(x,y):
+    return
 
 if __name__ == "__main__":
     N, M = map(int,input().split())
     D = [list(input()) for _ in range(N)]
-    print(D)
+    A = [[i,j] for i in range(N) for j in range(M) if D[i][j] == 'R' or D[i][j] == 'B' or D[i][j] == 'O']
+    print(A)
