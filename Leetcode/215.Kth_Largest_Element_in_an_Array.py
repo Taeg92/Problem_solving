f54422236f3cf484d835dc32ea05f69c6ef39d55
Taegyung Gong <gtg92t@gmail.com>
2020-12-23 17:25:43 +0900

    215. Kth Largest Element in an Array

---
+++ Leetcode/215.Kth_Largest_Element_in_an_Array.py
@@ -0,0 +1,37 @@
+import heapq
+
+
+# heapq 이용
+class Solution(object):
+    def findKthLargest(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        heapq.heapify(nums)
+
+        for _ in range(len(nums) - k):
+            heapq.heappop(nums)
+
+        return heapq.heappop(nums)
+
+
+# sort이용
+class Solution(object):
+    def findKthLargest(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        return sorted(nums, reverse=True)[k - 1]
+
+
+sol = Solution()
+
+nums = [3, 2, 1, 5, 6, 4]
+k = 2
+# Output: 5
+
+print(sol.findKthLargest(nums, k))
