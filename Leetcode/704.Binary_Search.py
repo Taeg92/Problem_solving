ba151f8dfdcb679b6b7e3176f3bc67d41cf48a44
Taegyung Gong <gtg92t@gmail.com>
2020-12-23 17:51:03 +0900

    704. Binary Search

---
+++ Leetcode/704.Binary_Search.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+
+            if nums[mid] < target:
+                left = mid + 1
+            elif nums[mid] > target:
+                right = mid - 1
+            else:
+                return mid
+        return -1
+
+
+sol = Solution()
+
+nums = [-1, 0, 3, 5, 9, 12]
+target = 9
+# Output: 4
+
+print(sol.search(nums, target))
