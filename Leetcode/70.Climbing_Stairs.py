71b014c646208704e1fc560e229c4930abdb3a2e
Taegyung Gong <gtg92t@gmail.com>
2021-02-12 11:13:47 +0900

    70. Climbing_Stairs

---
+++ Leetcode/70.Climbing_Stairs.py
@@ -0,0 +1,25 @@
+import collections
+
+
+class Solution(object):
+
+    dp = collections.defaultdict(int)
+
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n <= 2:
+            return n
+
+        if self.dp[n]:
+            return self.dp[n]
+        self.dp[n] = self.climbStairs(n - 1) + self.climbStairs(n - 2)
+        return self.dp[n]
+
+
+sol = Solution()
+n = 3
+
+print(sol.climbStairs(n))
