5e4d0ff6f0e7be3fc7d874952eab972d2b15d67b
Taegyung Gong <gtg92t@gmail.com>
2021-02-22 09:21:29 +0900

    69. Sqrt(x)

---
+++ Leetcode/69.Sqrt(x).py
@@ -0,0 +1,17 @@
+import math
+
+
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        return int(math.sqrt(x))
+
+
+x = 4
+
+sol = Solution()
+
+print(sol.mySqrt(x))
