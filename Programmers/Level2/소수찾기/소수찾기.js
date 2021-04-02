993ea05fbd0e9582d28c21a642c989527ef69164
Taegyung Gong <gtg92t@gmail.com>
2021-04-02 21:39:04 +0900

    Level 2. 소수 찾기 | JS

---
+++ Programmers/Level2/소수찾기/소수찾기.js
@@ -0,0 +1,20 @@
+function solution(n) {
+  let answer = Array(n + 1).fill(1);
+
+  for (let i = 2; i * i < n; i++) {
+    if (answer[i]) {
+      for (let j = 2; j * i <= n; j++) {
+        answer[i * j] = 0;
+      }
+    }
+  }
+  answer.shift();
+
+  let total = answer.reduce((acc, cur) => acc + cur);
+
+  return total - 1;
+}
+
+const n = 10;
+
+console.log(solution(n));
