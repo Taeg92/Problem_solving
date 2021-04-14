ea6592ade299e0082f40cc1e6c8accf9250afd56
Taegyung Gong <gtg92t@gmail.com>
2021-04-14 19:38:56 +0900

    Level 1. 같은 숫자는 싫어 | JS

---
+++ Programmers/Level1/같은숫자는싫어/같은숫자는싫어.js
@@ -0,0 +1,15 @@
+function solution(arr) {
+  const answer = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (answer[answer.length - 1] != arr[i]) {
+      answer.push(arr[i]);
+    }
+  }
+
+  return answer;
+}
+
+const arr = [1, 1, 3, 3, 0, 1, 1];
+
+console.log(solution(arr));
