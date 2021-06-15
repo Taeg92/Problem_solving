e6873a6419dd1b12b981a356fcb0ff2795e4ba3e
Taegyung Gong <gtg92t@gmail.com>
2021-06-15 19:27:04 +0900

    Level 2. 영어 끝말잇기 | JS

---
+++ Programmers/Level2/영어끝말잇기/영어끝말잇기.js
@@ -0,0 +1,28 @@
+function solution(n, words) {
+  let answer = 0;
+  for (let i = 1; i < words.length; i++) {
+    if (
+      words.indexOf(words[i]) !== i ||
+      words[i - 1][words[i - 1].length - 1] !== words[i][0]
+    ) {
+      answer = i + 1;
+      break;
+    }
+  }
+  return answer ? [answer % n || n, Math.ceil(answer / n)] : [0, 0];
+}
+
+const n = 3;
+const words = [
+  "tank",
+  "kick",
+  "know",
+  "wheel",
+  "land",
+  "dream",
+  "mother",
+  "robot",
+  "tank",
+];
+
+console.log(solution(n, words));
