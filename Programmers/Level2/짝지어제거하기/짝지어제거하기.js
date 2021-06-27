02088b5196e592836a43144dd097d0456a1c17a6
Taegyung Gong <gtg92t@gmail.com>
2021-06-27 21:43:53 +0900

    Level 2. 짝지어 제거하기 | JS

---
+++ Programmers/Level2/짝지어제거하기/짝지어제거하기.js
@@ -0,0 +1,16 @@
+function solution(s) {
+  const stack = [];
+
+  for (let i = 0; i < s.length; i++) {
+    if (!stack.length || stack[stack.length - 1] !== s[i]) {
+      stack.push(s[i]);
+    } else {
+      stack.pop();
+    }
+  }
+
+  return stack.length ? 0 : 1;
+}
+
+const s = "baabaa";
+console.log(solution(s));
