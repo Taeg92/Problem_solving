a1c759be350e7aceadfecaedd2134eb054538123
Taegyung Gong <gtg92t@gmail.com>
2021-07-02 21:15:27 +0900

    Level 2. 구명보트 | JS

---
+++ Programmers/Level2/구명보트/구명보트.js
@@ -0,0 +1,13 @@
+function solution(people, limit) {
+  var answer = 0;
+  people = people.sort((a, b) => b - a);
+
+  for (let i = 0, j = people.length - 1; i <= j; i++, answer++)
+    if (people[i] + people[j] <= limit) j--;
+
+  return answer;
+}
+
+const people = [70, 50, 80, 50];
+const limit = 100;
+console.log(solution(people, limit));
