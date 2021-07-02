5e8fbc7d929f71c5f12992e206226ad5145fcda4
Taegyung Gong <gtg92t@gmail.com>
2021-07-03 05:15:46 +0900

    Level 2. 문자열 압축 | PYTHON

---
+++ Programmers/Level2/문자열압축/문자열압축.py
@@ -0,0 +1,36 @@
+def solution(s):
+
+    answer = list()
+
+    if len(s) == 1:
+        return 1
+
+    for unit in range(1, len(s)//2 + 1):
+
+        cnt = 1
+        cut = s[:unit]
+        temp = ''
+
+        for i in range(unit, len(s), unit):
+            word = s[i:i+unit]
+            if cut == word:
+                cnt += 1
+            else:
+                if cnt == 1:
+                    temp += cut
+                else:
+                    temp += str(cnt) + cut
+                cut = word
+                cnt = 1
+        if cnt == 1:
+            temp += cut
+        else:
+            temp += str(cnt) + cut
+
+        answer.append(len(temp))
+
+    return min(answer)
+
+
+s = "aabbaccc"
+print(solution(s))
