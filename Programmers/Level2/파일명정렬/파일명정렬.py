e756bde778ed06a12855fc0097a299a7775a20be
Taegyung Gong <gtg92t@gmail.com>
2021-07-21 00:43:08 +0900

    Level 2. 파일명 정렬 | PYTHON

--- Programmers/Level2/파일명정렬/파일명정렬.py
+++ Programmers/Level2/파일명정렬/파일명정렬.py
@@ -3,7 +3,7 @@ import re
 
 def solution(files):
     arr = [re.split("(\d+)", file) for file in files]
-    return ["".join(a) for a in sorted(arr, key=lambda x: (x[0].lower(), int(x[1])))]
+    return ["".join(sorted_arr) for sorted_arr in sorted(arr, key=lambda x: (x[0].lower(), int(x[1])))]
 
 
 files = ["img12.png", "img10.png", "img02.png",
